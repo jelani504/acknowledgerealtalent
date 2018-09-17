9efc5dd767bf94b3d7747e20b01eabc22291b4db
Jelani Hankins <Jelani.Hankins@ge.com>
2018-09-17 16:07:06 -0500

    (add) import userModule, export auth service

--- art-server/src/shared/shared.module.ts
+++ art-server/src/shared/shared.module.ts
@@ -3,10 +3,12 @@ import { ConfigService } from './config/config.service';
 import { MapperService } from './mapper/mapper.service';
 import { AuthService } from './auth/auth.service';
 import { JwtStrategyService } from './auth/strategies/jwt-strategy.service';
+import { UserModule } from 'user/user.module';
 
 @Global()
 @Module({
   providers: [ConfigService, MapperService, AuthService, JwtStrategyService],
-  exports: [ConfigService, MapperService],
+  exports: [ConfigService, MapperService, AuthService],
+  imports: [UserModule],
 })
 export class SharedModule {}
