fc54a4df22eff1548a515767c72c62eb709becb9
Jelani Hankins <Jelani.Hankins@ge.com>
2018-09-17 14:40:43 -0500

    (add) typegoose class

--- art-server/src/shared/base.service.ts
+++ art-server/src/shared/base.service.ts
@@ -1,9 +1,10 @@
 import 'automapper-ts/dist/automapper';
 import { Document, Model, Types } from 'mongoose';
+import { Typegoose, ModelType, InstanceType } from 'typegoose';
 
-export class BaseService<T extends Document>{
+export class BaseService<T extends Typegoose>{
 
-    protected _model: Model<T>;
+    protected _model: ModelType<T>;
     protected _mapper: AutoMapperJs.AutoMapper;
     private get modelName(): string {
         return this._model.modelName;
@@ -13,7 +14,7 @@ export class BaseService<T extends Document>{
     }
 
     async map<K>(
-        object: Partial<T> | Partial<T>[],
+        object: Partial<InstanceType<T>> | Partial<InstanceType<T>>[],
         isArray: boolean = false,
         sourceKey?: string,
         destinationKey?: string,
@@ -25,28 +26,32 @@ export class BaseService<T extends Document>{
         return this._mapper.map(_sourceKey, _destinationKey, object);
     }
 
-    async findAll(filter = {}): Promise<T[]> {
+    async findAll(filter = {}): Promise<InstanceType<T>[]> {
         return this._model.find(filter).exec();
     }
 
-    async findOne(filter = {}): Promise<T> {
+    async findOne(filter = {}): Promise<InstanceType<T>> {
         return this._model.findOne(filter).exec();
     }
 
-    async findById(id: string): Promise<T> {
+    async findById(id: string): Promise<InstanceType<T>> {
         return this._model.findById(this.toObjectId(id)).exec();
     }
 
-    async create(item: T): Promise<T>{
+    async create(item: T): Promise<InstanceType<T>>{
         return this._model.create(item);
     }
 
-    async delete(id: string): Promise<T> {
+    async delete(id: string): Promise<InstanceType<T>> {
         return this._model.findByIdAndRemove(this.toObjectId(id)).exec();
     }
 
-    async update(id: string, item: T): Promise<T> {
+    async update(id: string, item: InstanceType<T>): Promise<InstanceType<T>> {
         return this._model.findByIdAndUpdate(this.toObjectId(id), item, { new: true }).exec();
+    }
+
+    async clearCollection(filter = {}): Promise<void> {
+        return this._model.deleteMany(filter).exec();
     }
 
     private toObjectId(id: string): Types.ObjectId {
