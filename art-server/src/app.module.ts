f85e050772944433c6184fcba15eacbe008e1a73
Jelani Hankins <Jelani.Hankins@ge.com>
2018-09-17 14:39:09 -0500

    (create) user module

--- art-server/src/app.module.ts
+++ art-server/src/app.module.ts
@@ -5,9 +5,10 @@ import { AppService } from './app.service';
 import { SharedModule } from './shared/shared.module';
 import { ConfigService } from 'shared/config/config.service';
 import { Config } from './shared/config/config.enum';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [SharedModule, MongooseModule.forRoot(ConfigService.connectionString, { useNewUrlParser: true })],
+  imports: [SharedModule, MongooseModule.forRoot(ConfigService.connectionString, { useNewUrlParser: true }), UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
