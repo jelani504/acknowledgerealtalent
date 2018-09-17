821baf49301bfe6feb8026bf5401a3f6a353d5b0
Jelani Hankins <Jelani.Hankins@ge.com>
2018-09-17 14:41:00 -0500

    (add) auth Service, jwt strat service

--- art-server/src/shared/shared.module.ts
+++ art-server/src/shared/shared.module.ts
@@ -1,10 +1,12 @@
 import { Module, Global } from '@nestjs/common';
 import { ConfigService } from './config/config.service';
 import { MapperService } from './mapper/mapper.service';
+import { AuthService } from './auth/auth.service';
+import { JwtStrategyService } from './auth/strategies/jwt-strategy.service';
 
 @Global()
 @Module({
-  providers: [ConfigService, MapperService],
+  providers: [ConfigService, MapperService, AuthService, JwtStrategyService],
   exports: [ConfigService, MapperService],
 })
 export class SharedModule {}
