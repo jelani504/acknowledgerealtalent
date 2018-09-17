d268634b0b1edc742b6b0b5567553b13ba06798d
Jelani Hankins <Jelani.Hankins@ge.com>
2018-09-17 16:08:02 -0500

    (add) validate payload

--- art-server/src/shared/auth/strategies/jwt-strategy.service.ts
+++ art-server/src/shared/auth/strategies/jwt-strategy.service.ts
@@ -1,4 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { AuthService } from '../auth.service';
+import { ConfigService } from '../../config/config.service';
+import { Strategy } from 'passport-strategy';
+import { ExtractJwt, VerifiedCallback } from 'passport-jwt';
+import { Config } from '../../config/config.enum';
+import { JwtPayload } from '../jwt-payload';
 
 @Injectable()
-export class JwtStrategyService {}
+export class JwtStrategyService extends PassportStrategy(Strategy, 'JwtStrategy') {
+    constructor(
+        private readonly _authService: AuthService,
+        private readonly _configService: ConfigService,
+    ){
+        super({
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            secretOrKey: _configService.get(Config.JWT_KEY),
+        });
+    }
+
+    async validate(payload: JwtPayload, done: VerifiedCallback) {
+        const user = await this._authService.validatePayload(payload);
+
+        if (!user){
+            return done(new HttpException({}, HttpStatus.UNAUTHORIZED), false);
+        }
+        return done(null, user, payload.iat);
+    }
+}
